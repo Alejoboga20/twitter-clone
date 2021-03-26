0b1362053d26fc43886b370b0f89474f79d1ee92
alejoboga <alejoboga20@gmail.com>
2021-03-26 16:12:04 -0300

    Styling Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,10 @@
-import { Avatar } from '@material-ui/core';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import React from 'react';
+import { Avatar } from '@material-ui/core';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import './Post.css';
 
 const Post = ({ avatar, displayName, username, verified, text, image }) => {
@@ -14,18 +18,24 @@ const Post = ({ avatar, displayName, username, verified, text, image }) => {
           <div className='post__headerText'>
             <h3>
               Header 3
-              <span>
-                <VerifiedUserIcon className='post__badge' />
+              <span className='post__headerSpecial'>
+                <VerifiedUserIcon className='post__badge' /> @alejoboga
               </span>
             </h3>
           </div>
           <div className='post__headerDescription'>
             <p>Twitter clone with react</p>
           </div>
-          <img
-            src='https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif'
-            alt=''
-          />
+        </div>
+        <img
+          src='https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif'
+          alt=''
+        />
+        <div className='post__footer'>
+          <ChatBubbleOutlineIcon fontSize='small' />
+          <RepeatIcon fontSize='small' />
+          <FavoriteBorderIcon fontSize='small' />
+          <PublishIcon fontSize='small' />
         </div>
       </div>
     </div>

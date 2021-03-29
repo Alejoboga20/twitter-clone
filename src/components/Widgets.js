74c73b78736dc5633b8dae1ba185973cca6b7f43
alejoboga <alejoboga20@gmail.com>
2021-03-29 12:23:13 -0300

    Adding widgets

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import {
+  TwitterShareButton,
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed
+} from 'react-twitter-embed';
 import './Widgets.css';
 
 const Widgets = () => {
   return (
     <div className='widgets'>
-      <h2>What's happening?</h2>
+      <div className='widgets__input'>
+        <SearchIcon className='widget__searchIcon' />
+        <input placeholder='Search Twitter' type='text' />
+      </div>
+      <div className='widget__widgetContainer'>
+        <h2>What's happening?</h2>
+        <TwitterTweetEmbed tweetId={'858551177860055040'} />
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='cleverqazi'
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={'https://facebook.com/cleverprogrammer'}
+          options={{ text: 'React is awsemo', via: 'cleverqazi' }}
+        />
+      </div>
     </div>
   );
 };

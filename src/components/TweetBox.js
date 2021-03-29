df93ce5768cd4d992c542d75b9229e117bb1fcf0
alejoboga <alejoboga20@gmail.com>
2021-03-29 15:42:32 -0300

    Adding tweetBox functionality

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, Button } from '@material-ui/core';
+import db from '../firebase';
 import './TweetBox.css';
 
 const TweetBox = () => {
+  const [tweetMessage, setTweetMessage] = useState('');
+  const [tweetImage, setTweetImage] = useState('');
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection('posts').add({
+      displayName: 'Alejo Boga',
+      username: 'aboga',
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar: 'https://miro.medium.com/max/554/1*Ld1KM2WSfJ9YQ4oeRf7q4Q.jpeg'
+    });
+  };
+
   return (
     <div className='tweetBox'>
       <form>
         <div className='tweetBox__input'>
           <Avatar src='https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png' />
-          <input type='text' placeholder="What's happening?" />
+          <input
+            type='text'
+            placeholder="What's happening?"
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+          />
         </div>
         <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
           className='tweetBox__imageInput'
           type='text'
           placeholder='Optional: Image Url'
         />
-        <Button className='tweetBox__tweeButton'>Tweet</Button>
+        <Button
+          onClick={sendTweet}
+          type='submit'
+          className='tweetBox__tweeButton'
+        >
+          Tweet
+        </Button>
       </form>
     </div>
   );
